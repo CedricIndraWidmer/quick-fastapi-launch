612f8ecb4e5ddef7ed7fdd02666c7ff697abfbc8
CedricIndraWidmer <82592422+CedricIndraWidmer@users.noreply.github.com>
2026-02-06 10:27:21 +0100

    added uvicorn lifecycle to app

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
+import uvicorn
+import asyncio
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return "EC2 system check"
+
+
+async def main():
+    config = uvicorn.Config(
+        "app:app", host="0.0.0.0", port=8000, log_level="info"
+    )
+    server = uvicorn.Server(config)
+    await server.serve()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
