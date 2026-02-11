c728fa8303a30cbe9f239ba623d25e618c103400
CedricIndraWidmer <82592422+CedricIndraWidmer@users.noreply.github.com>
2026-02-11 06:18:24 +0100

    update main to reflect dual env

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = FastAPI(root_path=os.getenv("ROOT_PATH", ""))
 
 @app.get("/")
 def read_root():
-    return "Github Actions system check successful 10.02 v.1!"
+    return "Github Actions system check successful. Running dual environments on 11.02 - v.1!"
