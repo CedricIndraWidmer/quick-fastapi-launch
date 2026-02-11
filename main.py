b011a932025640e9888221879a5c7ee7e5089fd8
CedricIndraWidmer <82592422+CedricIndraWidmer@users.noreply.github.com>
2026-02-11 08:30:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import os
+import datetime
 from fastapi import FastAPI
 
 app = FastAPI(root_path=os.getenv("ROOT_PATH", ""))
 
+version = "1.0.0"
+now = datetime.now()
+formatted_date = now.strftime("%Y.%m.%d")
+message = f"Github Actions system check successful. Running dual environments on {formatted_date} - v.{version}!"
+
+
+@app.get("/staging/")
+def read_root():
+    return message
+
+
+@app.get("/prod/")
+def read_root():
+    return message
+
 
 @app.get("/")
 def read_root():
-    return "Github Actions system check successful. Running dual environments on 11.02 - v.1!"
+    return "This is the root path. Please access /staging/ or /prod/ to see the message."
