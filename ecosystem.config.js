1896a0cbd8d1d7d0dfcc60d17f698d5047ab5bf8
CedricIndraWidmer <82592422+CedricIndraWidmer@users.noreply.github.com>
2026-02-11 08:16:16 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,26 @@
 module.exports = {
-    apps: [{
-        name: "fastapi-app",
-        script: "uvicorn",
-        // DO NOT put flags in the script field
-        // USE a space-separated string for args
-        args: "main:app --host 0.0.0.0 --port 8000",
-        interpreter: "python3",
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '1G',
-
-        env_staging: {
+    apps: [
+        {
             name: "fastapi-app-staging",
-            // If you want to change the port here, you must repeat the full args string
+            script: "uvicorn",
             args: "main:app --host 0.0.0.0 --port 8000",
-            NODE_ENV: "staging"
+            interpreter: "python3",
+            autorestart: true,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: "staging"
+            }
         },
-        env_production: {
+        {
             name: "fastapi-app-prod",
+            script: "uvicorn",
             args: "main:app --host 0.0.0.0 --port 8001",
-            NODE_ENV: "production"
+            interpreter: "python3",
+            autorestart: true,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: "production"
+            }
         }
-    }]
+    ]
 };
